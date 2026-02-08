8ca91c07ee1d0b6c1a2c2d4bf24d9d00e7ddd490
Test User <test@example.com>
2026-02-08 23:20:56 +0200

    refactor(13-01): rebrand shell wrapper templates to ptt
    - Rename wt() to ptt() in bash, zsh, and fish wrappers
    - Update case statements to intercept go|goto|home|mk|new|eject
    - Replace __WT_BIN__ with __PTT_BIN__ placeholder (3 occurrences per file)
    - Update binPlaceholder constant in embed.go to __PTT_BIN__

--- internal/shell/templates/wrapper.fish
+++ internal/shell/templates/wrapper.fish
@@ -1,16 +1,16 @@
-function wt
+function ptt
   switch $argv[1]
-    case goto home new eject
-      set -l result ("__WT_BIN__" --output-path $argv)
+    case go goto home mk new eject
+      set -l result ("__PTT_BIN__" --output-path $argv)
       set -l exit_code $status
       if test $exit_code -eq 0 -a -n "$result"
         cd "$result"; or return 1
       end
       return $exit_code
     case '*'
-      "__WT_BIN__" $argv
+      "__PTT_BIN__" $argv
   end
 end
 
 # Completions
-"__WT_BIN__" completion fish 2>/dev/null | source
+"__PTT_BIN__" completion fish 2>/dev/null | source
